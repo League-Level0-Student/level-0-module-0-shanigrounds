2f110d0933772132ddbe6f5e723df59e10698a0e
codeclub <codeclub@smms-rm23-33.smusd.local>
2018-02-07 14:58:53 -0800

    Level 0 Module 0

--- src/RobotSquare.java
+++ src/RobotSquare.java
@@ -11,22 +11,23 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
+Robot meow = new Robot();
 
         // 3. Put the robot's pen down
-
+meow.penDown();
 
         // 6. Make the robot move as fast as possible
-
+meow.setSpeed(1000);
 
         // 5. Do everything below here 4 times
-
+for(int x = 0; x < 4; x++) {
 
         //         2. Move your robot 200 pixels
-
+meow.move(200);
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
-
+meow.turn(90);
 
     }
+}
 }
